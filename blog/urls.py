9018cf224ec9ffefdfb162705c317da123f1dfcd
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-07-29 18:41:14 +0200

    added pagination and user posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
+    path('user/<str:username>/', UserPostListView.as_view(), name="user-posts"),
     path('about/', about, name="blog-about")
 ]
